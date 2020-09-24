1798dbe68f9bb47f442c47ef0997277a617e3076
Souma <souma.hitech@gmail.com>
2020-09-24 23:26:14 +0530

    Successfully Integrate Mongo Datadase.

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const Joi = require('joi');
+const mongoose = require('mongoose');
 const genres = require('./routes/genres');
 const express = require('express');
 const app = express();
+
+mongoose.connect('mongodb://localhost/playground',{ useNewUrlParser: true ,useUnifiedTopology: true,useFindAndModify:false})
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch((err) => console.err('Could not connect to MongoDB...'));
 
 app.use(express.json());
 app.use('/api/genres', genres);
