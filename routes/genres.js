f05be26bd72b42687180fe7f2aeb835a7fbe0518
Souma <souma.hitech@gmail.com>
2020-09-25 21:22:35 +0530

    extract genre model and put into a seperate module.

--- routes/genres.js
+++ routes/genres.js
@@ -1,18 +1,7 @@
+const {Genre,validate} = require('../models/genre');
 const mongoose = require('mongoose');
 const express = require('express');
-const Joi = require('joi');
 const router = express.Router();
-
-const genresSchema = new mongoose.Schema({
-  name:{
-    type:String,
-    required:true,
-    minlength:5,
-    maxlength:50
-  }
-});
-
-const Genre = mongoose.model('Genre',genresSchema);
 
 router.get('/', async (req, res) => {
   const genres = await Genre.find().sort('name');
@@ -20,7 +9,7 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  const { error } = validateGenre(req.body); 
+  const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
   let genre = new Genre({ name: req.body.name });
@@ -30,7 +19,7 @@ router.post('/', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
-  const { error } = validateGenre(req.body); 
+  const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
   const genre = await Genre.findByIdAndUpdate(req.params.id,{name:req.body.name},{
@@ -56,12 +45,5 @@ router.get('/:id', async (req, res) => {
   res.send(genre);
 });
 
-function validateGenre(genre) {
-  const schema = {
-    name: Joi.string().min(3).required()
-  };
-
-  return Joi.validate(genre, schema);
-}
 
 module.exports = router;
