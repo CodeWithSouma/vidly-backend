97c07f341434f4c4fcd0246e272aad02c417d1c9
Souma <souma.hitech@gmail.com>
2020-09-30 23:07:51 +0530

    extract validation logic and put into a seperate module.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const Joi = require('joi');
-Joi.objectId = require('joi-objectid')(Joi);
+const winston = require('winston');
 const express = require('express');
 const app = express();
 
@@ -7,9 +6,7 @@ require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config')();
-
-
-
+require('./startup/validation')();
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+app.listen(port, () => winston.info(`Listening on port ${port}...`));
