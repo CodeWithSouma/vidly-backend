34ccc98b5d496f002fe8982b168acb0819449eb8
Souma <souma.hitech@gmail.com>
2020-09-30 22:48:13 +0530

    extract logging functionality and put into a seperate module.

--- index.js
+++ index.js
@@ -1,27 +1,12 @@
-require('express-async-errors');
-const winston = require('winston');
-require('winston-mongodb');
 const config = require('config');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const express = require('express');
 const app = express();
 
+require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db')();
-
-winston.handleExceptions(
-    new winston.transports.File({filename:'uncaughtException.log'}));
-
-process.on('unhandledRejection',(ex) => {
-   throw ex;
-});
-
-winston.add( winston.transports.File,{ filename: 'logfile.log' });
-winston.add( winston.transports.MongoDB,{
-    db:'mongodb://localhost/vidly',
-    level:'error'
-});
 
 if (!config.get('jwtPrivateKey')) {
     console.error('FATAL ERROR:jwtPrivateKey is not defined.');
