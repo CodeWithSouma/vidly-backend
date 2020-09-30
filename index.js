4a9927d8fd023cb8d721975ea381672a3197a9a7
Souma <souma.hitech@gmail.com>
2020-09-30 19:24:06 +0530

    handle uncaught error and unhandle promise rejection

--- index.js
+++ index.js
@@ -15,12 +15,18 @@ const auth = require('./routes/auth');
 const express = require('express');
 const app = express();
 
-winston.add(new winston.transports.File({ filename: 'logfile.log' }));
-winston.add(new winston.transports.MongoDB({
+winston.handleExceptions(
+    new winston.transports.File({filename:'uncaughtException.log'}));
+
+process.on('unhandledRejection',(ex) => {
+   throw ex;
+});
+
+winston.add( winston.transports.File,{ filename: 'logfile.log' });
+winston.add( winston.transports.MongoDB,{
     db:'mongodb://localhost/vidly',
     level:'error'
-}));
-
+});
 
 if (!config.get('jwtPrivateKey')) {
     console.error('FATAL ERROR:jwtPrivateKey is not defined.');
--- middleware/error.js
+++ middleware/error.js
@@ -2,6 +2,6 @@ const winston = require('winston');
 
 module.exports = function(err,req,res,next){
     console.log(err);
-    winston.error({message: err.message, level: err.level, stack: err.stack, meta: err});
+    winston.error(err.message, err);
     res.status(500).send('Something failed.');
 }
