6e9cadbd615592fe5e486670d4123a6e49986250
Souma <souma.hitech@gmail.com>
2020-10-02 19:27:17 +0530

    add validateObjectId middleware to validate id parms of req

--- routes/genres.js
+++ routes/genres.js
@@ -1,4 +1,5 @@
 const auth = require('../middleware/auth');
+const validateObjectId = require('../middleware/validateObjectId');
 const admin = require('../middleware/admin');
 const {Genre,validate} = require('../models/genre');
 const mongoose = require('mongoose');
@@ -42,10 +43,7 @@ router.delete('/:id', [auth,admin], async (req, res) => {
   res.send(genre);
 });
 
-router.get('/:id', async (req, res) => {
-  if(!mongoose.Types.ObjectId.isValid(req.params.id))
-    return res.status(404).send('Invalid ID.');
-
+router.get('/:id', validateObjectId, async (req, res) => {
   const genre = await Genre.findById(req.params.id);
   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
   res.send(genre);
