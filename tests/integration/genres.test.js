df444b70aee600603d9252ec65ed6a4b24af8862
Souma <souma.hitech@gmail.com>
2020-10-02 18:46:41 +0530

    refactor genres get route test suit

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,14 +1,29 @@
 const request = require('supertest');
+const {Genre} = require('../../models/genre');
 let server;
 
 describe('/api/genres', () => {
     beforeEach(() => { server = require('../../index'); });
-    afterEach(() => { server.close(); });
+    afterEach(async () => { 
+        server.close(); 
+        await Genre.remove({});
+    });
 
     describe('GET /', () => {
         it('should return all genres', async () => {
+
+            await Genre.insertMany([
+                {name:'genre1'},
+                {name:'genre2'}
+            ]);
+
             const res = await request(server).get('/api/genres');
             expect(res.status).toBe(200);
+            expect(res.body.length).toBe(2);
+            expect(res.body.some(g => g.name === 'genre1')).toBeTruthy();
+            expect(res.body.some(g => g.name === 'genre2')).toBeTruthy();
         });
     });
+
+    
 });
