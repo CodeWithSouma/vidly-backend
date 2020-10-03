19c1fb191886b18ff3af5570ec16b391957ef64d
Souma <souma.hitech@gmail.com>
2020-10-04 01:42:33 +0530

    refactor return route

--- routes/returns.js
+++ routes/returns.js
@@ -1,14 +1,16 @@
+const Joi = require("joi");
 const express = require('express');
 const { Rental } = require('../models/rental');
 const auth = require('../middleware/auth');
 const moment = require('moment');
 const {Movie} = require('../models/movie');
+const validate = require("../middleware/validate");
 const router = express.Router();
 
 
-router.post('/', auth, async (req,res) => {
-    if(!req.body.customerId) return res.status(400).send('Customer id not provided.');
-    if(!req.body.movieId) return res.status(400).send('Movie id not provided.');
+
+router.post('/', [auth,validate(validateReturn)], async (req,res) => {
+
     const rental = await Rental.findOne({
         'customer._id':req.body.customerId,
         'movie._id':req.body.movieId
@@ -27,5 +29,15 @@ router.post('/', auth, async (req,res) => {
     return res.status(200).send(rental);
     
 });
+
+function validateReturn(req) {
+    const schema = {
+      customerId: Joi.objectId().required(),
+      movieId: Joi.objectId().required()
+    };
+  
+    return Joi.validate(req, schema);
+}
+
 
 module.exports = router;
