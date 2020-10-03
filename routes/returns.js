5dc486bb7160dadd5b6533bbc2be5f0b22f6320c
Souma <souma.hitech@gmail.com>
2020-10-04 01:57:57 +0530

    refactor returns endpoints

--- routes/returns.js
+++ routes/returns.js
@@ -8,13 +8,9 @@ const validate = require("../middleware/validate");
 const router = express.Router();
 
 
-
 router.post('/', [auth,validate(validateReturn)], async (req,res) => {
-
-    const rental = await Rental.findOne({
-        'customer._id':req.body.customerId,
-        'movie._id':req.body.movieId
-    });
+    const rental = await Rental.lookup(req.body.customerId,req.body.movieId);
+    
     if(!rental) return res.status(404).send('Rental not found.');
     if(rental.dateReturned) return res.status(400).send('Returned already processed.');
     rental.dateReturned = new Date();
