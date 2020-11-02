7800256bb0bf6ba4e814db1f404b60344a2784f4
Souma <souma.hitech@gmail.com>
2020-11-02 10:16:40 +0530

    modyfy payload of jwt and send more property in jwt

--- models/user.js
+++ models/user.js
@@ -1,49 +1,57 @@
-const config = require('config');
-const jwt = require('jsonwebtoken');
-const Joi = require('joi');
-const mongoose = require('mongoose');
+const config = require("config");
+const jwt = require("jsonwebtoken");
+const Joi = require("joi");
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name:{
-    type:String,
-    required:true,
-    minlength:5,
-    maxlength:50,
-    trim:true
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50,
+    trim: true,
   },
-  email:{
-      type:String,
-      required:true,
-      minlength:5,
-      maxlength:255,
-      trim:true,
-      unique:true
+  email: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+    trim: true,
+    unique: true,
   },
-  password:{
-      type:String,
-      required:true,
-      minlength:5,
-      maxlength:1024,
-      trim:true,
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 1024,
+    trim: true,
   },
-  isAdmin:Boolean
+  isAdmin: Boolean,
 });
 
 userSchema.methods.generateAuthToken = function () {
-  const token = jwt.sign({_id:this._id,isAdmin:this.isAdmin},config.get('jwtPrivateKey'));
+  const token = jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      name: this.name,
+      isAdmin: this.isAdmin,
+    },
+    config.get("jwtPrivateKey")
+  );
   return token;
-}
-  
-const User = mongoose.model('User',userSchema);
+};
+
+const User = mongoose.model("User", userSchema);
 
 function validateUser(user) {
-    const schema = {
-      name: Joi.string().min(5).max(50).required(),
-      email: Joi.string().min(5).max(255).required().email(),
-      password: Joi.string().min(5).max(255).required()
-    };
-  
-    return Joi.validate(user, schema);
+  const schema = {
+    name: Joi.string().min(5).max(50).required(),
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(255).required(),
+  };
+
+  return Joi.validate(user, schema);
 }
 
 module.exports.User = User;
